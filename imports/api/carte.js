ef8253e6c55864a22973d9a198ea63eda0b3994a
Benjamin <benjulienne0@gmail.com>
2018-11-05 09:29:34 +0400

    version 5

--- imports/api/carte.js
+++ imports/api/carte.js
@@ -1,2 +1,3 @@
 import { Mongo } from 'meteor/mongo';
 export const Cartes = new Mongo.Collection('carte');
+export const Reserves = new Mongo.Collection('reserves');
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,8 @@ import { Inscrits } from '../api/col.js';
 
 import { Cartes } from '../api/carte.js';
 
+import { Reserves } from '../api/carte.js';
+
 // Importe les Templates depuis 'meteor/templating'
 
 import { Template } from 'meteor/templating';
@@ -19,6 +21,10 @@ import './body.html';
 import './template.js';
 
 // Affiche les inscrits dans l'atelier
+Template.body.onCreated(function bodyOnCreated() {
+    // counter starts at 0
+    this.counter = new ReactiveVar(0);
+  });
 
 Template.body.helpers({
 inscription() {
@@ -30,9 +36,22 @@ carte() {
     // Show newest tasks at the top
     return Cartes.find({}, { sort: { createdAt: -1 } });
 },
+
+reserves() {
+    return Reserves.find({}, {sort: { createdAt: -1 } });
+},
+
+counter() {
+    return Template.instance().counter.get();
+  },
 });
 
 Template.body.events({
+
+'click .reserv'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+},
 
 'submit .formulair'(event) {
 
@@ -47,11 +66,13 @@ Template.body.events({
     const prenoM = target.prenom.value;
     const phonE = target.phone.value;
     const maiL = target.mail.value;
+    const atelieR = target.choixAtelier.value;
 
 
     // Insérer une tâche dans la collection
 
     Inscrits.insert({
+    atelieR,
     noM,
     prenoM,
     phonE,
@@ -59,8 +80,9 @@ Template.body.events({
     createdAt: new Date(), // heure actuelle
     });
 
-    // Forme claire
+    // efface les valeurs dans les inputs
 
+    target.choixAtelier.value = '';
     target.nom.value = '';
     target.prenom.value = '';
     target.phone.value = '';
@@ -80,18 +102,32 @@ Template.body.events({
     const Picture = target.picture.value;
     const Title = target.title.value;
     const Describ = target.describ.value;
+    const Dates = target.date.value;
+    const Horaires = target.horaire_de_debut.value;
+    const Durée = target.durée.value;
+    const Placedispo = target.Nombre_de_place_disponible.value;
+    const Prix = target.prix.value;
 
     Cartes.insert({
         Picture,
         Title,
         Describ,
+        Dates,
+        Horaires,
+        Durée,
+        Placedispo,
+        Prix,
         createdAt: new Date(), // heure actuelle
         });
 
         target.picture.value = '';
         target.title.value = '';
         target.describ.value = '';
-
+        target.date.value = '';
+        target.horaire_de_debut.value = '';
+        target.durée.value = '';
+        target.Nombre_de_place_disponible.value = '';
+        target.prix.value = '';
 
 
 
@@ -105,16 +141,49 @@ Template.body.events({
     const Picture2 = target.picture2.value;
     const Title2 = target.title2.value;
     const Describ2 = target.describ2.value;
+    const Dates2 = target.date2.value;
+    const Horaires2 = target.horaire_de_debut2.value;
+    const Durée2 = target.durée2.value;
+    const Placedispo2= target.Nombre_de_place_disponible2.value;
+    const Prix2 = target.prix2.value;
     const id = target.editId.value;
 
     Cartes.update(id, {
-        $set: {Picture: Picture2, Title: Title2, Describ: Describ2}
+        $set: {Picture: Picture2, Title: Title2, Describ: Describ2, Dates: Dates2, Horaires: Horaires2, Durée: Durée2, Placedispo: Placedispo2, Prix: Prix2 }
     });
 
     $('#modalModif').modal('hide');
 },
 
+/* Utilise une collection pour ajouté une reservation depuis un modal. */
 
+'submit .reservation'(event) {
+
+    event.preventDefault();
+
+    const target = event.target;
+    const Name = target.nom.value;
+    const firstName = target.prenom.value;
+    const Mail = target.mail.value;
+    const Phone = target.phone.value;
+    const titre2 = target.title11.value;
+
+    Reserves.insert({
+        titre2,
+        Name,
+        firstName,
+        Mail,
+        Phone,
+        createdAt: new Date(),
+    });
+
+    $('#modal3').modal('hide');
+
+    target.nom.value = '';
+    target.prenom.value = '';
+    target.mail.value = '';
+    target.phone.value = '';
+},
 
 'click .btn-edit-modif' (event) {
 
@@ -122,46 +191,73 @@ Template.body.events({
     const idModif = target.getAttribute('data-id');
     const modif = Cartes.findOne({_id:idModif});
 
+/*Permet de recupérer l'id des inputs*/ 
+   
     const firstModif = document.querySelector('#modif1');
     const secondModif = document.querySelector('#modif2');
     const thirdModif = document.querySelector('#modif3');
+    const fourthModif = document.querySelector('#modif4');
+    const fiveModif = document.querySelector('#modif5');
+    const sixModif = document.querySelector('#modif6');
+    const sevenModif = document.querySelector('#modif7');
+    const eightModif = document.querySelector('#modif8');
     const hidden = document.querySelector('#edit-id');
+
+/*lie les inputs du formulaire au modal*/ 
 
     hidden.value = idModif;
     firstModif.value = modif.Picture;
     secondModif.value = modif.Title;
     thirdModif.value = modif.Describ;
+    fourthModif.value = modif.Dates;
+    fiveModif.value = modif.Horaires;
+    sixModif.value = modif.Durée;
+    sevenModif.value = modif.Placedispo;
+    eightModif.value = modif.Prix;
+
 },
+
+
+'click .btn-edit-reserve'(event) {
+
+    const target = event.target;
+    const idModif = target.getAttribute('data-id');
+    const modif = Cartes.findOne({_id:idModif});
+
+/*Permet de recupérer l'id des inputs*/
+
+    const hidden = document.querySelector('#edit-id');
+    const secondModif = document.querySelector('#modif11');
+    const thirdModif = document.querySelector('#modif3');
+
+  
+    hidden.value = modif._id;
+    secondModif.value = modif.Title;
+    thirdModif.value = modif.Describ;
+
+},
+
 
 });
 
-
+/*supprime le membre de la collection Inscrits*/ 
 Template.fred.events({
     'click .deleted'() {
     Inscrits.remove(this._id);
     },
 
- /*
-   'click .btn-edit-membre'(event) {
-
-    const target = event.target;
-    const idMembre = target.getAttribute('data-id');
-    const membre = Collect.findOne({_id:idMembre});
-
-    const firstmod = document.querySelector("#titre");
-    const secondmod = document.querySelector("#titreCarte");
-    const thirdmod = document.querySelector("#texte");
-    const fourmod = document.querySelector("#liste");
-    const hidden = document.querySelector("#editId");
-
-    hidden.value = idMembre;
-
-    }, */
 
 });
-
+/*supprime le membre de la collection Cartes*/ 
 Template.cartes.events({
     'click .suppr'() {
     Cartes.remove(this._id);
     },
+
+});
+/*supprime le membre de la collection Reserves*/
+Template.Reservation.events({
+    'click .supprime'() {
+        Reserves.remove(this._id);
+        },
 });
